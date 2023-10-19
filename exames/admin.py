d5e4a5f193700960ef1c42d479141783cc49a8d2
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-19 16:15:29 -0300

    migrate SolicitacaoExames para db
    Foi criado e exportado para o banco de dados a tabela de solicitação de exames.

--- exames/admin.py
+++ exames/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TiposExames
+from .models import TiposExames, SolicitacaoExame
 
 # Register your models here.
 admin.site.register(TiposExames)
+admin.site.register(SolicitacaoExame)
--- exames/models.py
+++ exames/models.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TiposExames(models.Model):
@@ -16,3 +17,16 @@ class TiposExames(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class SolicitacaoExame(models.Model):
+    choice_status = (
+        ('E', 'Em análise'),
+        ('F', 'Finalisado')
+    )
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    exame = models.ForeignKey(TiposExames, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=choice_status)
+    resultado = models.FileField(upload_to="resultados", null=True, blank=True)
+    requer_senha = models.BooleanField(default=False)
+    senha = models.CharField(max_length=16, null=True, blank=True)
