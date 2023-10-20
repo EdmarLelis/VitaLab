b6c47c53adb16a26bcc412d4e91006950b8194c9
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-19 21:43:32 -0300

    pagina de solicitação de exames style
    Parte estética da pagina de solicitação de exames.

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+@login_required
+def solicitar_exames(request):
+    if request.method == 'GET':
+        return render(request, 'solicitar_exames.html')
+#2. 00:18:20 ...
