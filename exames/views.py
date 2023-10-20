6f44bbe8cbe621d1209357feb844d347c123b268
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-20 02:37:06 -0300

    Solicitação de exames
    desenvolvimento da página de solicitação de exames.

--- exames/views.py
+++ exames/views.py
@@ -1,14 +1,25 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages import constants
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-
+from .models import TiposExames
 # Create your views here.
 
 @login_required
 def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all()
     if request.method == 'GET':
-        return render(request, 'solicitar_exames.html')
-#2. 00:18:20 ...
+        print(tipos_exames)
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
+    elif request.method == 'POST':
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+
+        preco_total = 0
+        for i in solicitacao_exames:
+            if i.disponivel == True:
+                preco_total += i.preco
+        print(preco_total)
+
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames, 
+                                                         'solicitacao_exames': solicitacao_exames,
+                                                         'preco_total': preco_total})
