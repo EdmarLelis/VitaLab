77da9733814610103f12868afa04021f5a53f80f
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-21 00:37:19 -0300

    fechar pedido
    Adiciona no banco de dados o pedido solicitado.

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
+from datetime import datetime
 # Create your views here.
 
 @login_required
@@ -15,6 +16,7 @@ def solicitar_exames(request):
         solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
 
         preco_total = 0
+        data = datetime.now()
         for i in solicitacao_exames:
             if i.disponivel == True:
                 preco_total += i.preco
@@ -22,4 +24,27 @@ def solicitar_exames(request):
 
         return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames, 
                                                          'solicitacao_exames': solicitacao_exames,
-                                                         'preco_total': preco_total})
+                                                         'preco_total': preco_total,
+                                                         'data': data})
+    
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
+    solicitacao_exames = TiposExames.objects.filter(id__in = exames_id)
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now()
+    )
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario = request.user,
+            exame = exame,
+            status= "E"
+        )
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+
+    pedido_exame.save()
+
+    return HttpResponse('url funciona')
