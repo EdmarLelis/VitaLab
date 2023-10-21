053a639f8dbd30e9fac65e2783ffc3572c109c0c
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-21 00:58:46 -0300

    validação de pedidos.
    Criação de sistema que valida se um pedido pode ser efetuado ou não com base na disponibilidade dos exames adicionados.

--- exames/views.py
+++ exames/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import TiposExames, PedidosExames, SolicitacaoExame
 from datetime import datetime
+from django.contrib.messages import constants
+from django.contrib import messages
 # Create your views here.
 
 @login_required
@@ -30,21 +32,33 @@ def solicitar_exames(request):
 def fechar_pedido(request):
     exames_id = request.POST.getlist('exames')
     solicitacao_exames = TiposExames.objects.filter(id__in = exames_id)
+    valido = False
+    for exame in solicitacao_exames:
+        if exame.disponivel == True:
+            valido = True
     pedido_exame = PedidosExames(
         usuario = request.user,
         data = datetime.now()
     )
-    pedido_exame.save()
+    if valido == True:
+        pedido_exame.save()
 
-    for exame in solicitacao_exames:
-        solicitacao_exames_temp = SolicitacaoExame(
-            usuario = request.user,
-            exame = exame,
-            status= "E"
-        )
-        solicitacao_exames_temp.save()
-        pedido_exame.exames.add(solicitacao_exames_temp)
+    if valido == True:
+        for exame in solicitacao_exames:
+            if exame.disponivel == True:
+                solicitacao_exames_temp = SolicitacaoExame(
+                    usuario = request.user,
+                    exame = exame,
+                    status= "E"
+                )
+            solicitacao_exames_temp.save()
+            pedido_exame.exames.add(solicitacao_exames_temp)
 
-    pedido_exame.save()
+        pedido_exame.save()
+        messages.add_message(request, constants.SUCCESS, 'Pedido de exames realizado com sucesso!')
+    
+    else:
+        messages.add_message(request, constants.ERROR, 'Pedido Invalido, o(s) exame(s) solicitado(s) não estão disponíveis')
 
-    return HttpResponse('url funciona')
+    valido = False
+    return redirect('/exames/ver_pedidos')
