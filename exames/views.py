477980fa7c5dcab80b4c9dc888c3207c0920eefb
EdmarLelisLourenco <120860142+EdmarLelis@users.noreply.github.com>
2023-10-24 14:45:06 -0300

    senha exame
    validação de senha para acessar exames adicionada.

--- exames/views.py
+++ exames/views.py
@@ -104,3 +104,11 @@ def solicitar_senha_exame(request, exame_id):
     exame = SolicitacaoExame.objects.get(id = exame_id)
     if request.method == "GET":
         return render(request, 'solicitar_senha_exame.html', {'exame':exame})
+    
+    elif request.method == 'POST':
+        senha = request.POST.get('senha')
+        if senha == exame.senha:
+            return redirect(exame.resultado.url)
+        else:
+            messages.add_message(request, constants.ERROR, 'Senha incorreta.')
+            return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
